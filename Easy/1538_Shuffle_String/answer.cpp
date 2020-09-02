ef72d6b77e644641586f20bca1873df5fab20d1d
CptBang <muashims@gmail.com>
2020-09-01 22:38:39 -0700

    1538 done in O(1) space

--- Easy/1538_Shuffle_String/answer.cpp
+++ Easy/1538_Shuffle_String/answer.cpp
@@ -16,3 +16,18 @@ public:
         return ret;
     }
 };
+
+// no additional space allocated, but it looks so nasty
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        for (int i = 0; i < indices.size(); i++) {
+            while (indices[i] != i) {
+                swap(s[i], s[indices[i]]);
+                swap(indices[i], indices[indices[i]]);
+            }
+        }
+        
+        return s;
+    }
+};
